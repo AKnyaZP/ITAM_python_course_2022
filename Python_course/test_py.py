8d5989395b2bf34bc6257345903e5cdb88d648e7
AKnyaZP <113966078+AKnyaZP@users.noreply.github.com>
2022-10-15 14:49:26 +0300

    Delete Python_course directory

--- Python_course/test_py.py
+++
@@ -1,9 +0,0 @@
-def hello(name, surname):
-    return f'Доброго времени суток, {name} "Человек" {surname}!'
-
-a = str(input())
-a = a.split(' ')
-name = a[0]
-surname = a[1]
-print (hello(name, surname))
-#№1
